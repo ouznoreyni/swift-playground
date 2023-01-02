ae92f2081e7429953d189446db9b566238f4071f
Ousmane DIOP <ousmanediop1297@gmail.com>
2023-01-02 13:05:53 +0000

    finish i rich project

---
+++ i am rich/i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Ousmane diop on 02/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
